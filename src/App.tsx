6857b326db543572e9f6b350ae0bb24d5686bfc8
Steve Heyes <steve@substrakthealth.com>
2020-08-15 16:48:26 +0100

    ADD: Basic camera

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={Camera.Constants.Type.front}>
+
+      </Camera>
+    </View>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import App from "./App";
+
+class AppWrapper extends PureComponent {
+  render() {
+    return <App />;
+  }
+}
+
+export default AppWrapper;
